a33a409816ba3db26d64c5de3f974fca8f480877
Michael Hoffmann <michael.hoffmann@sentry.io>
2025-06-24 02:44:09 +0200

    Revert "SDK debug"
    This reverts commit 6ee3401b82fd40849445cf500feafb0c171bd2b4.

--- main.go
+++ main.go
@@ -166,7 +166,6 @@ func PostHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	err := sentry.Init(sentry.ClientOptions{
 		Dsn:              os.Getenv("SENTRY_DSN"),
-		Debug:            true,
 		EnableTracing:    true,
 		TracesSampleRate: 1.0,
 	})
